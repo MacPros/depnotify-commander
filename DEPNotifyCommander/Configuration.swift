5e4faa42a990120d9a68378728929907850cdf4d
Eric Summers <eric_summers@icloud.com>
2022-04-16 20:01:21 -0400

    Wrap additional DEPNotify features.

--- DEPNotifyCommander/Configuration.swift
+++ DEPNotifyCommander/Configuration.swift
@@ -11,14 +11,46 @@ class Configuration: Decodable {
     
     var steps: [DEPNotifyStep]
     
+    // The initial content.
+    var content: DEPNotifyContent?
+    
+    // The initial status.
+    var status: String?
+    
+    // Replace the default DEPNotify icon.
+    var icon: String?
+    
+    // The content to show on completion.
+    var completionContent: DEPNotifyContent?
+    
+    // Show a completion button after the last step.
+    var completionButton: String?
+    
+    // Run a policy before exiting. This will run before the button is pressed.
+    var completionEvent: String?
 }
 
 class DEPNotifyStep: Decodable {
     
-    /// Change the status.
-    var status: String
-    
     /// The custom trigger to invoke.
-    var event: String
+    var event: String?
     
+    var status: String?
+    
+    var content: DEPNotifyContent?
+    
+}
+
+class DEPNotifyContent: Decodable {
+    var title: String?
+    
+    var text: String?
+    
+    var image: String?
+    
+    var video: String?
+    
+    var youTube: String?
+    
+    var website: String?
 }
--- DEPNotifyCommander/main.swift
+++ DEPNotifyCommander/main.swift
@@ -47,14 +47,86 @@ do {
 
 print("Starting DEPNotify workflow.")
 
+if let icon = configuration.icon {
+    depnotify.image = .path(icon)
+}
+
+if let content = configuration.content {
+    content.update()
+}
+
 do {
     for step in configuration.steps {
-        print("Starting policies with event: \(step.event)")
-        depnotify.status = step.status
-        try shellOut(to: "/usr/local/bin/jamf", arguments: ["policy", "-event", step.event])
+        if let status = step.status {
+            print("Status: \(status)")
+            depnotify.status = step.status ?? ""
+        }
+        
+        if let content = step.content {
+            content.update()
+        }
+        
+        if let event = step.event {
+            print("Starting policies with event: \(event)")
+            try shellOut(to: "/usr/local/bin/jamf", arguments: ["policy", "-event", event])
+        }
     }
+    
+    if let completionContent = configuration.completionContent {
+        completionContent.update()
+    }
+    
+    if let completionButton = configuration.completionButton {
+        depnotify.showQuitButton(buttonLabel: completionButton)
+    }
+    
+    if let completionEvent = configuration.completionEvent {
+        try shellOut(to: "/usr/local/bin/jamf", arguments: ["policy", "-event", completionEvent])
+    }
+    
 } catch {
     fatalError("Error communicating with jamf command line. \(error)")
 }
 
 print("DEPNotify workflow complete.")
+
+// MARK: - Helpers
+
+extension DEPNotifyContent {
+    public func update() {
+        if let title = title {
+            depnotify.title = title
+        }
+        if let text = text {
+            depnotify.content = .text(text)
+        }
+        
+        if let image = image, image != "" {
+            depnotify.content = .image(.path(image))
+        }
+        
+        if let video = video, video != "" {
+            var vid: DEPNotify.Video? = nil
+            if video.hasPrefix("http") {
+                if let url = URL(string: video) {
+                    vid = .url(url)
+                } else {
+                    vid = .path(video)
+                }
+            }
+            if let vid = vid {
+                depnotify.content = .video(vid)
+            }
+        }
+        
+        if let youTube = youTube, youTube != "" {
+            depnotify.content = .video(.youTube(id: youTube))
+        }
+        
+        if let website = website, website != "" {
+            if let url = URL(string: website) {
+                depnotify.content = .website(url)
+            }
+        }
+    }
+}
